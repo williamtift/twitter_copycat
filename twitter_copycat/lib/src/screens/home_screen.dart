f390106a651901248485c852246d8761e8ba1a44
William Tift <williamtift@outlook.com>
2022-01-12 12:59:57 -0300

    Avance en el homeScreen. Modifique colores, agregue bottom nav bar, etc.

--- twitter_copycat/lib/src/screens/home_screen.dart
+++ twitter_copycat/lib/src/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:twitter_copycat/src/colores.dart';
 import '../datatypes.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -30,76 +31,126 @@ class _HomeScreenState extends State<HomeScreen> {
           widget.signOut();
         },
       ),
-      ListTile(
-        leading: Icon(Icons.settings),
-        title: Text('Otra opcion 1'),
-        onTap: () {},
-      ),
-      ListTile(
-        leading: Icon(Icons.group),
-        title: Text('Otra opcion 2'),
-        onTap: () {},
-      ),
-      ListTile(
-        leading: Icon(Icons.add),
-        title: Text('Etc'),
-        onTap: () {},
-      ),
     ];
+  }
+
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    Center(
+      child: Text(
+        'Home',
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      ),
+    ),
+    Center(
+      child: Text(
+        'Search',
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      ),
+    ),
+    Center(
+      child: Text(
+        'Notifications',
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      ),
+    ),
+    Center(
+      child: Text(
+        'Messages',
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      ),
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          title: FaIcon(FontAwesomeIcons.twitter,
-              color: Colors.blueAccent, size: 35),
-        ),
-        //Drawer
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-                  DrawerHeader(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).primaryColor,
-                    ),
-                    child: Column(children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text(
-                            'Settings',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 24,
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () => {
-                              Navigator.of(context).pop(),
-                            },
-                            iconSize: 30.0,
-                            icon: const Icon(
-                              Icons.arrow_back,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ]),
-                  ),
-                ] +
-                _generarOpcionesDrawer(),
-          ),
-        ),
-        body: ListView(
-          padding: const EdgeInsets.all(10),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.blueAccent,
+        elevation: 2,
+        title: FaIcon(FontAwesomeIcons.twitter,
+            size: 35),
+      ),
+      //Drawer
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
           children: <Widget>[
-            Text("Hola"),
-          ],
-        ));
+                DrawerHeader(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  child: Column(children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'Settings',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () => {
+                            Navigator.of(context).pop(),
+                          },
+                          iconSize: 30.0,
+                          icon: const Icon(
+                            Icons.arrow_back,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ]),
+                ),
+              ] +
+              _generarOpcionesDrawer(),
+        ),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(10),
+        children: <Widget>[
+          _widgetOptions.elementAt(_selectedIndex),
+        ],
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications_outlined),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.email_outlined),
+            label: "",
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blueAccent,
+        unselectedItemColor: colorGrisDeImagenPrueba,
+        iconSize: 32,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
